6373f6eb681426895b3e6370bf7fd427edb1a6af
gdlden <ghyilcr@163.com>
2025-02-23 10:46:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git config --global --add safe.directory /github/workspace
+tree .
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/**.apk -m "v${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
